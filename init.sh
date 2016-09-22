1415aa1f690a09d3c6a499d9505c1aa84b21aa11
Fractal Zombie <fractalzombie@gmail.com>
2016-09-22 19:46:36 +0300

    “Changes”

--- init.sh
+++ init.sh
@@ -7,5 +7,8 @@ mkdir -p "$homesteadRoot"
 cp -i src/stubs/Homestead.yaml "$homesteadRoot/Homestead.yaml"
 cp -i src/stubs/after.sh "$homesteadRoot/after.sh"
 cp -i src/stubs/aliases "$homesteadRoot/aliases"
+ln -s "$(pwd)"/scripts "$homesteadRoot/scripts"
+ln -s "$(pwd)"/Vagrantfile "$homesteadRoot/Vagrantfile"
+ln -s "$(pwd)"/.vagrant "$homesteadRoot/.vagrant"
 
 echo "Homestead initialized!"
