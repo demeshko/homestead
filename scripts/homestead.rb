3b9b5b2db25bc844ef39b128054d2dc6cb224244
Fractal <fractalzombie@gmail.com>
2016-09-22 19:12:01 +0300

    Update for Shared Folders to boost up it

--- scripts/homestead.rb
+++ scripts/homestead.rb
@@ -126,7 +126,7 @@ class Homestead
         mount_opts = []
 
         if (folder["type"] == "nfs")
-            mount_opts = folder["mount_options"] ? folder["mount_options"] : ['actimeo=1', 'nolock']
+            mount_opts = folder["mount_options"] ? folder["mount_options"] : ['actimeo=1', 'nolock', 'vers=3', 'udp', 'noatime']
         elsif (folder["type"] == "smb")
             mount_opts = folder["mount_options"] ? folder["mount_options"] : ['vers=3.02', 'mfsymlinks']
         end
